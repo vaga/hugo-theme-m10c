f68c75babd3c0e04891c166bbca0add956d63fee
Alex Norell <alex@norell.co>
2024-07-26 11:42:10 -0700

    Add lightbox navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
 function initLightbox() {
     const galleryItems = document.querySelectorAll(".lightbox-image");
     const lightbox = createLightbox();
+    let currentIndex = 0;
 
-    galleryItems.forEach(item => {
+    galleryItems.forEach((item, index) => {
         item.addEventListener("click", e => {
             e.preventDefault();
+            currentIndex = index;
             const imgSrc = item.getAttribute("href");
             const imgTitle = item.getAttribute("title");
-            updateLightbox(lightbox, imgSrc, imgTitle);
+            updateLightbox(lightbox, imgSrc, imgTitle, currentIndex, galleryItems.length);
         });
     });
 
@@ -21,11 +23,29 @@ function initLightbox() {
         lightbox.classList.remove("active");
     });
 
-    lightbox.addEventListener("click", e => {
-        if (e.target.tagName !== "IMG" && e.target.className !== "title") {
-            lightbox.classList.remove("active");
+    lightbox.querySelector("img").addEventListener("click", () => {
+        lightbox.classList.remove("active");
+    });
+
+    document.addEventListener("keydown", e => {
+        if (lightbox.classList.contains("active")) {
+            if (e.key === "ArrowRight" || e.key === "l") {
+                navigateLightbox(1);
+            } else if (e.key === "ArrowLeft" || e.key === "h") {
+                navigateLightbox(-1);
+            } else if (e.key === "Escape") {
+                lightbox.classList.remove("active");
+            }
         }
     });
+
+    function navigateLightbox(direction) {
+        currentIndex = (currentIndex + direction + galleryItems.length) % galleryItems.length;
+        const currentItem = galleryItems[currentIndex];
+        const imgSrc = currentItem.getAttribute("href");
+        const imgTitle = currentItem.getAttribute("title");
+        updateLightbox(lightbox, imgSrc, imgTitle, currentIndex, galleryItems.length);
+    }
 }
 
 function createLightbox() {
@@ -49,7 +69,7 @@ function createLightbox() {
     return lightbox;
 }
 
-function updateLightbox(lightbox, imgSrc, imgTitle) {
+function updateLightbox(lightbox, imgSrc, imgTitle, currentIndex, totalItems) {
     const img = lightbox.querySelector("img");
     const title = lightbox.querySelector(".title");
 
@@ -57,6 +77,7 @@ function updateLightbox(lightbox, imgSrc, imgTitle) {
     title.innerText = imgTitle;
     lightbox.classList.add("active");
 }
+
 
 function wrapHeadersWithLinks() {
     document.querySelectorAll("h2, h3, h4, h5, h6").forEach(header => {
